b444c6e3500d2f48ecfc13ae6484df79fde79c19
Mimic55 <aidenoconnor55@gmail.com>
2026-07-11 12:02:11 -0400

    Added absolute and relative acceleration. Created ImuManager for data gathering.

--- DAVE Hardware/include/DataStructures.h
+++ DAVE Hardware/include/DataStructures.h
@@ -21,10 +21,11 @@ struct ArmSegmentState {
     uint32_t timestamp;         // Milliseconds since boot
     uint8_t i2cAddress;         // 0x68 (Forearm) or 0x69 (Bicep)
     
+    Vector3D absoluteAccel;    // Raw accelerometer data (m/s^2)
     Quaternion4D orientation;   // AHRS fused orientation
     Vector3D angularVelocity;   // Raw gyro data (rad/s)
     Vector3D angularAccel;      // Derived angular acceleration (rad/s^2)
-    Vector3D linearAccel;       // Accel minus gravity vector (m/s^2)
+    Vector3D relativeAccel;       // Accel minus gravity vector (m/s^2)
     Vector3D gravityVector;     // Extracted gravity direction
     Vector3D position;          // Experimental tracking placeholder
 };
